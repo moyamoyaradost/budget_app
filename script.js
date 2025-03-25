5e3a7c950303f5a865f42c53dc2dfb7457c3a233
Max Novikhin <novihinm@yandex.ru>
2025-03-25 16:12:24 +0300

    Добавлен второй урок

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-// Запрашиваем бюджет на месяц и дату
-let money = prompt("Ваш бюджет на месяц?", '');
-let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+'use strict';
 
-// Создаем объект appData
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
 let appData = {
     budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true // Изменено на true для примера, может быть изменено в зависимости от требований
 };
 
-// Запрашиваем обязательные расходы (по два раза)
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", '');
-let a2 = prompt("Во сколько обойдется?", '');
-let a3 = prompt("Введите обязательную статью расходов в этом месяце", '');
-let a4 = prompt("Во сколько обойдется?", '');
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let b = prompt("Во сколько обойдется?", "");
 
-// Записываем данные в объект expenses
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        i--;
+    }
+}
 
-// Вычисляем бюджет на 1 день (при 30-дневном месяце) и выводим его
-alert("Бюджет на 1 день: " + (appData.budget / 30));
+appData.moneyPerDay = appData.budget / 30;
+alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+if (appData.moneyPerDay < 100) {
+    console.log("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Это высокий уровень достатка!");
+} else {
+    console.log("Произошла ошибка");
+}
+
+// Дополнительный функционал (пример):
+let howMuchCanSave = prompt("Сколько вы можете сэкономить в месяц?");
+if (appData.savings) {
+    let save = +howMuchCanSave,
+        percent = 10; // Допустим, процент от сбережений
+    alert("Ваш ежемесячный доход с депозита: " + (save / 100 / 12 * percent));
+}
